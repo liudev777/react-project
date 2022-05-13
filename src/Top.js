58beadc03fd1e957d22a2714a107c7b8be04dc2b
Devin Liu <dliu44@hawk.iit.edu>
2022-05-13 15:35:10 -0500

    remove folder

---
+++ src/Top.js
@@ -0,0 +1,17 @@
+function Top() {
+    return (
+      <div>
+        <ul> 
+          <li>
+            hey
+          </li>
+          <li>
+            lmao  
+          </li>
+        </ul>
+        hey
+      </div>
+    );
+  }
+  
+  export default Top;

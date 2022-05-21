50f45feeedcec534c677c03db3087cf169c66d30
Devin Liu <dliu44@hawk.iit.edu>
2022-05-21 11:45:02 -0500

    0521 update

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,28 +1,35 @@
 import locationLogo from "../images/Path.png"
+import data from "../data.js"
+
 
 export default function Card(props) {
     return (
-        <div className="card">
-            <img src={props.item.imageUrl} className="card--image"/>
-            <div className="card--stats">
-                <div className="map--row">
-                    <img src={locationLogo} alt="location marker"/>
-                    <h5 className="location--name">{props.item.location}</h5>
-                    <a href={props.item.googleMapsUrl} className="map--url">View on Google Maps</a>
+        <>
+            <div className="card">
+                <img src={props.item.imageUrl} className="card--image"/>
 
+                <div className="card--stats">
+                    <div className="map--row">
+                        <img src={locationLogo} alt="location marker"/>
+                        <h5 className="location--name">{props.item.location}</h5>
+                        <a href={props.item.googleMapsUrl} className="map--url">View on Google Maps</a>
+                    </div>
+
+                    <h1 className="card--title">
+                        {props.item.title}
+                    </h1>
+
+                    <h3 className="card--date">
+                        {props.item.startDate} - {props.item.endDate}
+                    </h3>
+                    
+                    <p className="card--description">
+                        {props.item.description}
+                    </p>
                 </div>
-
-                <h1 className="card--title">
-                    {props.item.title}
-                </h1>
-                <h3 className="card--date">
-                    {props.item.startDate} - {props.item.endDate}
-                </h3>
-                
-                <p className="card--description">
-                    {props.item.description}
-                </p>
             </div>
-        </div>
+            {/* {console.log(data.length)} */}
+            {props.item.id < data.length && <hr/>}
+        </>
     );
 }
